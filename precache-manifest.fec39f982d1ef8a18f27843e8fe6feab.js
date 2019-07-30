c04552681d94194bf3233017248927bf4f317ec1
AnJaWr <october3@o2.pl>
2019-07-30 11:40:44 +0200

    Updates

---
+++ precache-manifest.fec39f982d1ef8a18f27843e8fe6feab.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a45326ae494cac4b7e7db5e487e8bbc1",
+    "url": "/AleaQW/index.html"
+  },
+  {
+    "revision": "d7e768f78b1546d78686",
+    "url": "/AleaQW/static/css/3.510409cd.chunk.css"
+  },
+  {
+    "revision": "c2d69bef1fe0782acb3f",
+    "url": "/AleaQW/static/css/main.478c5719.chunk.css"
+  },
+  {
+    "revision": "1bff1ef1fc369c199272",
+    "url": "/AleaQW/static/js/2.3562a7ca.chunk.js"
+  },
+  {
+    "revision": "d7e768f78b1546d78686",
+    "url": "/AleaQW/static/js/3.7d8151a4.chunk.js"
+  },
+  {
+    "revision": "96d735a663ae92025581",
+    "url": "/AleaQW/static/js/4.1da27873.chunk.js"
+  },
+  {
+    "revision": "c2d69bef1fe0782acb3f",
+    "url": "/AleaQW/static/js/main.8973d179.chunk.js"
+  },
+  {
+    "revision": "8dd31f4e2f49fda3bc59",
+    "url": "/AleaQW/static/js/runtime~main.bf1fca72.js"
+  },
+  {
+    "revision": "0c67b094946d43534b47fae8e8b5b885",
+    "url": "/AleaQW/static/media/k10.0c67b094.jpg"
+  },
+  {
+    "revision": "1d0591d5cad6bce732f3e208944f943a",
+    "url": "/AleaQW/static/media/k10.1d0591d5.jpg"
+  },
+  {
+    "revision": "215804bb9d48e4f783953ebcc4576015",
+    "url": "/AleaQW/static/media/k10.215804bb.jpg"
+  },
+  {
+    "revision": "bfb7d2e5a6abad8b6e1408857d94aee5",
+    "url": "/AleaQW/static/media/k10.bfb7d2e5.jpg"
+  },
+  {
+    "revision": "243417bfc6d9a3051072c9ae213269a2",
+    "url": "/AleaQW/static/media/k100.243417bf.jpg"
+  },
+  {
+    "revision": "53ccf09bc87d1a2636e0d3ba9247a2ea",
+    "url": "/AleaQW/static/media/k100.53ccf09b.jpg"
+  },
+  {
+    "revision": "949b1b44b9b0f2c4c68dd32b379858c5",
+    "url": "/AleaQW/static/media/k100.949b1b44.jpg"
+  },
+  {
+    "revision": "a62b6a3157d537ba9a694eb874ad3395",
+    "url": "/AleaQW/static/media/k100.a62b6a31.jpg"
+  },
+  {
+    "revision": "fc96434e59b6880bf19cf7f34bc03f9c",
+    "url": "/AleaQW/static/media/k100.fc96434e.jpg"
+  },
+  {
+    "revision": "32350828aa99d67ae2556dd88bf60cf1",
+    "url": "/AleaQW/static/media/k12.32350828.jpg"
+  },
+  {
+    "revision": "8cbc38d4b69fe4ebd5138c1a77f010ac",
+    "url": "/AleaQW/static/media/k12.8cbc38d4.jpg"
+  },
+  {
+    "revision": "c697825ff937fdf9e817d8768f1d26e8",
+    "url": "/AleaQW/static/media/k12.c697825f.jpg"
+  },
+  {
+    "revision": "db85c1bea4d435f696dafb2bc91e512a",
+    "url": "/AleaQW/static/media/k12.db85c1be.jpg"
+  },
+  {
+    "revision": "f4acab1a56e056bcf801b253bc7dcd68",
+    "url": "/AleaQW/static/media/k12.f4acab1a.jpg"
+  },
+  {
+    "revision": "373d8e38d658fec51b26cf69f61883c8",
+    "url": "/AleaQW/static/media/k20.373d8e38.jpg"
+  },
+  {
+    "revision": "3986c975c657edd30e0e9760f5d2b762",
+    "url": "/AleaQW/static/media/k20.3986c975.jpg"
+  },
+  {
+    "revision": "66e757ee76cfb44a4b0b0f1c99ce8054",
+    "url": "/AleaQW/static/media/k20.66e757ee.jpg"
+  },
+  {
+    "revision": "85efd4ac664f8e00990dca2b3a76cc6c",
+    "url": "/AleaQW/static/media/k20.85efd4ac.jpg"
+  },
+  {
+    "revision": "a9a7f1e59973c607a688cb38c6a9530c",
+    "url": "/AleaQW/static/media/k20.a9a7f1e5.jpg"
+  },
+  {
+    "revision": "4f95b3e4760e2a00662146b6db909ca9",
+    "url": "/AleaQW/static/media/k4.4f95b3e4.jpg"
+  },
+  {
+    "revision": "5e5cdc94eea8034647724954aff90caa",
+    "url": "/AleaQW/static/media/k4.5e5cdc94.jpg"
+  },
+  {
+    "revision": "79ca1d303dacabc8ab8b3e1763702ccd",
+    "url": "/AleaQW/static/media/k4.79ca1d30.jpg"
+  },
+  {
+    "revision": "6a93356ad7420e56631d0bd497421317",
+    "url": "/AleaQW/static/media/k6.6a93356a.jpg"
+  },
+  {
+    "revision": "713230607055b21937945febda8abf51",
+    "url": "/AleaQW/static/media/k6.71323060.jpg"
+  },
+  {
+    "revision": "a6016c463cf7e58e99df97bfc47cf577",
+    "url": "/AleaQW/static/media/k6.a6016c46.jpg"
+  },
+  {
+    "revision": "a93d4f43b0a7db998c6ffd49edf3e079",
+    "url": "/AleaQW/static/media/k6.a93d4f43.jpg"
+  },
+  {
+    "revision": "d402f6325ee62bea6ceff0f70fc25a86",
+    "url": "/AleaQW/static/media/k6.d402f632.jpg"
+  },
+  {
+    "revision": "7feb3e3c11be2c9f79cf94f940ff610a",
+    "url": "/AleaQW/static/media/k8.7feb3e3c.jpg"
+  },
+  {
+    "revision": "a7840525986bb608ef3f462100eefbb9",
+    "url": "/AleaQW/static/media/k8.a7840525.jpg"
+  },
+  {
+    "revision": "cec98234a9d76c9fa80d23ff64ac3b11",
+    "url": "/AleaQW/static/media/k8.cec98234.jpg"
+  },
+  {
+    "revision": "cf25d2776df3ffc3f658caa94f8359b0",
+    "url": "/AleaQW/static/media/k8.cf25d277.jpg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/AleaQW/precache-manifest.fec39f982d1ef8a18f27843e8fe6feab.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/AleaQW/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
